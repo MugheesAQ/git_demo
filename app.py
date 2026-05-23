b3ddaf45716c0ab4d9816e1d2d4b0679a4142322
mugheesAQ <mugheesaq404@gmail.com>
2026-05-23 11:18:56 +0500

    added code

---
+++ app.py
@@ -0,0 +1,30 @@
+print("===== Calculator =====")
+print("1. Addition")
+print("2. Multiplication")
+print("3. Exit")
+
+choice = int(input("Enter choice: "))
+
+if choice == 1:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Sum =", a + b)
+
+elif choice == 2:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    print("Multiplication =", a * b)
+
+elif choice == 3:
+    print("Goodbye!")
+
+else:
+    print("Invalid choice")
+
+print("""
+WAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+WAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+BETEEEEEEEEEEEE MOUJJJJJJJJJJJ KERDIIIIIIIIIII
+WAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+WAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+""")
